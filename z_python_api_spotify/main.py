951416678c32846ece7712e8eabd004492f2749d
hungnguyenthanh <nguyenthhung2k2@gmail.com>
2023-09-08 14:40:58 +0700

    ok

---
+++ z_python_api_spotify/main.py
@@ -0,0 +1,60 @@
+import os
+import base64
+from requests import post
+import json
+
+
+#  CLIENT_ID = "f887354b358e4a29a30e36e7dacb940b"
+# CLIENT_SECRET = "5ecb31487f384e68a8c5accc8d173eea"
+
+client_id = "f887354b358e4a29a30e36e7dacb940b"
+client_secret = "5ecb31487f384e68a8c5accc8d173eea"
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url,headers=headers,data = data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return {'Authorization:' "Bearer " + token}
+
+def search_for_artist(token,artist_name):
+    url = "htpps://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}$type=artist$limit=1"
+
+    query_url = url + query
+    result = get(query_url,headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+
+    if len(json_result) == 0:
+        print("No artist with this name exists...")
+        return None
+    
+    return json_result[0]
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url,headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+token = get_token
+result = search_for_artist(token,"ACDC")
+artist_id = result["id"]
+songs = get_songs_by_artist(token,artist_id)
+
+for idx,song in enumerate(songs):
+    print(f"{idx + 1}. {song['name']}")
